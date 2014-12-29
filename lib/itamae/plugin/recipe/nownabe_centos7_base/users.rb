abcfa994b378726dbf4db2490fc186baea49bf26
nownabe <nownabe@gmail.com>
2014-12-30 00:09:11 +0900

    FIX bug

--- lib/itamae/plugin/recipe/nownabe_centos7_base/users.rb
+++ lib/itamae/plugin/recipe/nownabe_centos7_base/users.rb
@@ -33,36 +33,39 @@ ALLOW_USERADD_OPTIONS = %w(
   end
 
   authorized_keys_file = "#{home_dir}/.ssh/authorized_keys"
+  does_put_authorized_keys = opt["authorized_keys"].present? or
+                             ( opt["authorized_keys_file"].present? and 
+                               File.exist?(File.expand_path(opt["authorized_keys_file"])) )
+
 
   execute "add user #{user}" do
     command cmd.join(" ")
     not_if "id #{user}"
-    if opt["authorized_keys"].present? or
-       ( opt["authorized_keys_file"].present? and 
-         File.exist?(File.expand_path(opt["authorized_keys_file"])) )
-
+    if does_put_authorized_keys 
       notifies :create, "directory[#{home_dir}/.ssh]"
       notifies :create, "file[#{authorized_keys_file}]"
     end
   end
  
-  directory "#{home_dir}/.ssh" do
-    owner user
-    group opt["gid"] || user
-    mode "0700"
-    action :nothing
-  end
- 
-  file authorized_keys_file do
-    if opt["authorized_keys"].present?
-      content opt["authorized_keys"]
-    else
-      content_file File.expand_path(opt["authorized_keys_file"])
+  if does_put_authorized_keys
+    directory "#{home_dir}/.ssh" do
+      owner user
+      group opt["gid"] || user
+      mode "0700"
+      action :nothing
     end
-    owner user
-    group opt["gid"] || user
-    mode "0600"
-    action :nothing
+ 
+    file authorized_keys_file do
+      if opt["authorized_keys"].present?
+        content opt["authorized_keys"]
+      else
+        content_file File.expand_path(opt["authorized_keys_file"])
+      end
+      owner user
+      group opt["gid"] || user
+      mode "0600"
+      action :nothing
+    end
   end
 
 end
--- lib/itamae/plugin/recipe/nownabe_centos7_base/version.rb
+++ lib/itamae/plugin/recipe/nownabe_centos7_base/version.rb
@@ -2,7 +2,7 @@ module Itamae
   module Plugin
     module Recipe
       module NownabeCentos7Base
-        VERSION = "0.0.2"
+        VERSION = "0.0.3"
       end
     end
   end
